0fca06ab731940ec371b4e033cc3ae695f1c2a48
SerenKaradag <srn488@gmail.com>
2021-07-30 10:39:16 -0500

    Seventh BRANCH(feature)- push 1 commit

--- src/day5/Conflict.java
+++ src/day5/Conflict.java
@@ -1,10 +1,4 @@
 package day5;
 
 public class Conflict {
-    public static void main(String[] args) {
-
-        //Sixth commit
-        //seventh commit
-        // eighth commit
-    }
 }
---
+++ src/day5/TC001.java
@@ -0,0 +1,5 @@
+package day5;
+
+public class TC001 {
+    //1 commit
+}
